28f1f56d3b3b6c1d0167430fc3133eb2f90ce4f9
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-05-23 15:50:40 +0530

    xx

--- catalogue.tf
+++ catalogue.tf
@@ -1,18 +1,18 @@
-resource "aws_instance" "frontned" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = ["sg-0c433bcccf94c7d36"]
   instance_type = "t3.small"
 
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
 
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "catalogue" {
   zone_id = "Z08573782K96CIOPZ7P6V"
-  name    = "frontend-dev"
+  name    = "catalogue-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.catalogue.private_ip]
 }
--- mongodb.tf
+++ mongodb.tf
@@ -1,18 +1,18 @@
-resource "aws_instance" "frontned" {
+resource "aws_instance" "mongodb" {
   ami           = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = ["sg-0c433bcccf94c7d36"]
   instance_type = "t3.small"
 
   tags = {
-    Name = "frontend"
+    Name = "mongodb"
   }
 }
 
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "mongodb" {
   zone_id = "Z08573782K96CIOPZ7P6V"
-  name    = "frontend-dev"
+  name    = "mongodb-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.mongodb.private_ip]
 }
