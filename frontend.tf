cdebd07c19ad8b109b636c999575957338981fb7
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-05-23 15:57:20 +0530

    ss

--- frontend.tf
+++ frontend.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "frontned" {
+resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = ["sg-0c433bcccf94c7d36"]
   instance_type = "t3.small"
