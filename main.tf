2bfef8fa1b0c004a47c28537a11f136c22426c6f
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-06-05 12:08:46 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-resource "aws_instance" "catalogue" {
-  ami           = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = ["sg-0c433bcccf94c7d36"]
-  instance_type = "t3.small"
+resource "aws_instance" "instance" {
+  count = length(var.instances)
+  ami           = var.ami_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  instance_type = var.instance_type
 
   tags = {
-    Name = "catalogue"
+    Name = var.instances[count.index]
+      
   }
 
 }
 
 
-
-
-
 resource "aws_route53_record" "catalogue" {
-  zone_id = "Z08573782K96CIOPZ7P6V"
-  name    = "catalogue-dev"
+  count = length(var.instances)
+  zone_id = var.zone_id
+  name    = "${var.instances[count.index]}-${var.env}"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.catalogue.private_ip]
+  records = [aws_instance.instance[count.index].private_ip]
 }
 
 resource "null_resource" "catalogue" {
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "ami_id"{
+    default="ami-09c813fb71547fc4f"
+}
+
+variable "instance_type"{
+    default="t3.small"
+}
+
+variable "vpc_security_group_ids"{
+    default= ["sg-0c433bcccf94c7d36"]
+}
+
+variable "instances"{
+    default= [
+        "catalogue",
+        "frontend",
+        "mongodb"
+    ]
+}
+
+variable "zone_id"{
+    default="Z08573782K96CIOPZ7P6V"
+}
+
+variable "env"{
+    default="dev"
+}
