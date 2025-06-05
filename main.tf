366ea970f930ee90dcb237ba72d8f08315027968
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-06-05 12:08:46 +0530

    ss

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "instance" {
+  count = length(var.instances)
+  ami           = var.ami_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instances[count.index]
+      
+  }
+
+}
+
+
+resource "aws_route53_record" "catalogue" {
+  count = length(var.instances)
+  zone_id = var.zone_id
+  name    = "${var.instances[count.index]}-${var.env}"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance[count.index].private_ip]
+}
