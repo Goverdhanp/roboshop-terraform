7ad5c763b53ab96f1a79598cbddd33e61313a4df
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-06-08 13:42:37 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,21 +1,41 @@
 resource "aws_instance" "instance" {
-  count = length(var.instances)
+  for_each=var.instances
   ami           = var.ami_id
-  vpc_security_group_ids = var.vpc_security_group_ids
   instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  
 
   tags = {
-    Name = var.instances[count.index]
+    Name = var.instances[each.key]
       
   }
 
 }
 
 resource "aws_route53_record" "catalogue" {
-  count = length(var.instances)
+  for_each= var.instances
   zone_id = var.zone_id
-  name    = "${var.instances[count.index]}-${var.env}"
+  name    = "${var.instances[each.key]}-${var.env}"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.instance[count.index].private_ip]
+  records = [aws_instance.instance[each.key].private_ip]
+}
+
+ 
+resource "null_resource" "catalogue" {
+    
+  provisioner "remote-exec" {
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = aws_instance.catalogue.private_ip
+    }
+
+    inline = [
+      "pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/Goverdhanp/roboshop-ansible.git roboshop.yml -e component_name=catalogue -e env=dev",
+    ]
+  }
+
 }
