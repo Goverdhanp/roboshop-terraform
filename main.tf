919b7d17b8ad108d3aaa2a992e0c315542b6c8ae
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-06-08 13:48:05 +0530

    cc

--- main.tf
+++ main.tf
@@ -22,20 +22,3 @@ resource "aws_route53_record" "catalogue" {
 }
 
  
-resource "null_resource" "catalogue" {
-    
-  provisioner "remote-exec" {
-    connection {
-        type     = "ssh"
-        user     = "ec2-user"
-        password = "DevOps321"
-        host     = aws_instance.catalogue.private_ip
-    }
-
-    inline = [
-      "pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/Goverdhanp/roboshop-ansible.git roboshop.yml -e component_name=catalogue -e env=dev",
-    ]
-  }
-
-}
