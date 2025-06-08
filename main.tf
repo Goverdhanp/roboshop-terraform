227d9d95c9ebe35803b497b213ce520863e3d520
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-06-08 13:58:34 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" {
-  for_each = var.instances
+  for_each = toset(var.instances)
   ami           = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
