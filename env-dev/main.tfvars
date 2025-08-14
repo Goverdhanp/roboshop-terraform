244764b01cc8f058267550bd42502e3d7fb2278f
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-08-14 11:35:26 +0530

    ss

---
+++ env-dev/main.tfvars
@@ -0,0 +1,20 @@
+instances = {
+        frontend={
+            ami_id = "ami-09c813fb71547fc4f"
+            instance_type = "t3.small"
+        }
+        catalogue = {
+            ami_id = "ami-09c813fb71547fc4f"
+            instance_type = "t3.small"
+        }
+        mongodb = {
+            ami_id = "ami-09c813fb71547fc4f"
+            instance_type = "t3.small"
+        }
+
+
+zone_id = "Z08573782K96CIOPZ7P6V"
+
+vpc_security_group_ids = ["sg-0c433bcccf94c7d36"]
+
+env="dev"
--- variables.tf
+++ variables.tf
@@ -1,29 +1,4 @@
-
-variable "vpc_security_group_ids"{
-    default= ["sg-0c433bcccf94c7d36"]
-}
-
-variable "instances"{
-    default = {
-        frontend={
-            ami_id = "ami-09c813fb71547fc4f"
-            instance_type = "t3.small"
-        }
-        catalogue = {
-            ami_id = "ami-09c813fb71547fc4f"
-            instance_type = "t3.small"
-        }
-        mongodb = {
-            ami_id = "ami-09c813fb71547fc4f"
-            instance_type = "t3.small"
-        }
-}
-}
-
-variable "zone_id"{
-    default="Z08573782K96CIOPZ7P6V"
-}
-
-variable "env"{
-    default="dev"
-}
+variable {}
+variable "instances"{}
+variable "zone_id"{}
+variable "env"{}
