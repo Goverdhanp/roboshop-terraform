0af8b5478ecb828d7f83307ba99097a96643b26a
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-08-20 13:42:20 +0530

    cc

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,38 +1,35 @@
-resource "aws_instance" "instance"{
-    ami = var.ami_id
-    instance_type= var.instance_type
-    vpc_security_group_ids = var.vpc_security_group_ids
+resource "aws_instance" "instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
 
-    tags{
-        Name = var.name
+  tags = {
+    Name = var.name
+  }
+}
+
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-${var.env}"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance.private_ip]
+}
+
+resource "null_resource" "catalogue" {
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
     }
 
+    inline = [
+      "sudo pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/raghudevopsb83/roboshop-ansible roboshop.yml -e component_name=${var.name} -e env=${var.env}",
+    ]
+  }
 }
-
-resource "aws_route53_record" "record"{
-    zone_id =var.zone_id
-    name= "${var.name}-${var.env}"
-    type = "A"
-    ttl= 10
-    records=[aws_instance.instance.private_ip]
-}
-
-resource "null_resource" "catalogue"{
-    depends_on = [aws_route53_recor ]
-    provisioner "remote-exec{
-        connection{
-            type = "ssh"
-            user= "ec2-user"
-            password= "DevOPS321"
-            host= aws_instance.instance.private_ip
-        }
-
-        inline= [
-            "sudo pip3.11 install ansible".
-            "ansible-pull -i localhost, -U https://github.com/raghudvopsb83/roboshop-ansible roboshop.yml -e component_name=$(var.name) -e env=$(var.env)",
-
-        ]
-
-    }
-}
-
